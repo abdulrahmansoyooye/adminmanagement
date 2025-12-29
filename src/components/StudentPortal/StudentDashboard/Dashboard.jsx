d45fac2a67e2578c89220090571672f4735b4582
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2025-12-29 22:36:06 +0100

    fix: remove unused imports in Dashboard component

--- src/components/StudentPortal/StudentDashboard/Dashboard.jsx
+++ src/components/StudentPortal/StudentDashboard/Dashboard.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import ProfileOverview from "./ProfileOverview";
-<<<<<<< HEAD
-//import PrintIDCard from "./PrintIDCard";
-import IDcardDetails from "../IDcardDetails";
-import LatestNews from "./LatestNews";
-=======
 
->>>>>>> 96dcf5a9f50d0e6f99c66f310be0c65e4a5ba6c4
-// import Header from "./Header";
 
 const Dashboard = () => {
   return (
