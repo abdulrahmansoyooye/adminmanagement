d24d359975b7badb9ff1d5ecee708c09ebde6ecb
Mubarahkah <abdulazeezolayinka1@gmail.com>
2026-01-05 09:16:13 +0100

    commit

--- src/components/StudentPortal/StudentDashboard/Dashboard.jsx
+++ src/components/StudentPortal/StudentDashboard/Dashboard.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import ProfileOverview from "./ProfileOverview";
-<<<<<<< HEAD
-//import PrintIDCard from "./PrintIDCard";
-import IDcardDetails from "../IDcardDetails";
-import LatestNews from "./LatestNews";
-=======
-
->>>>>>> 96dcf5a9f50d0e6f99c66f310be0c65e4a5ba6c4
 // import Header from "./Header";
 
 const Dashboard = () => {
   return (
+  
     <div className="">
       {/* <Header /> */}
       <div className="">
